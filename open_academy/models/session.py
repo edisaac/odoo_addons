e7c7624fbbd7e30968be9f4300ed1e98d9939e62
Isaac Mejia <edisaac.mejia@gmail.com>
2017-01-14 15:29:59 -0500

    instructor filter

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -10,4 +10,5 @@ class OpenAcademySession(models.Model):
     star_date = fields.Date("Star_Date")
     course_id = fields.Many2one("openacademy.course", string="Course")
     attendee_ids = fields.One2many("openacademy.attendee", 'session_id', string='attendees')
-    instructor_id = fields.Many2one("res.partner", string='Instructor')
+    instructor_id = fields.Many2one("res.partner", string='Instructor',
+                                    domain=[("is_instructor", '=', True)])
