61dd13a215952d0a781ecc5ea3999dd0e1ff9c8c
Isaac Mejia <edisaac.mejia@gmail.com>
2017-01-14 09:41:54 -0500

    relation with other modules

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -7,5 +7,6 @@ class OpenAcademyCourse(models.Model):
     name = fields.Char("Name", size=32, required=True)
     description = fields.Text("Description")
     session_ids = fields.One2many("openacademy.session", 'course_id', string='sessions')
+    responsible_id = fields.Many2one("res.users", string='Responsible')
 
     #python-chart pydot pyparsing pyd usb>=1.0.0b1
--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -10,3 +10,4 @@ class OpenAcademySession(models.Model):
     star_date = fields.Date("Star_Date")
     course_id = fields.Many2one("openacademy.course", string="Course")
     attendee_ids = fields.One2many("openacademy.attendee", 'session_id', string='attendees')
+    instructor_id = fields.Many2one("res.partner", string='Instructor')
